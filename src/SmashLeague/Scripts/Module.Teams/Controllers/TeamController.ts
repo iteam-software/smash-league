e2314ec59768dc4ae0cafb36136fb24abcaadf22
iteam-software <mlynam@iteamnm.com>
2015-08-17 19:51:14 -0600

    Teams and TeamController

---
+++ src/SmashLeague/Scripts/Module.Teams/Controllers/TeamController.ts
@@ -0,0 +1,18 @@
+﻿
+module SmashLeague.Teams {
+  'use strict';
+
+  export class TeamController {
+
+    public static $inject = [
+      '$state'
+    ];
+
+    constructor(
+      state: ng.ui.IStateService) {
+      
+    }
+  }
+
+  Application.Module.controller('TeamController', TeamController);
+}
