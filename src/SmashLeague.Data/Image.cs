f162c07662c95dc2e2d95e2330e026ffeb9ad81e
Matt Lynam <mlynam@iteamnm.com>
2015-08-18 08:26:25 -0600

    fixed git cache

---
+++ src/SmashLeague.Data/Image.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmashLeague.Data
+{
+    public class Image
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProfileImageId { get; set; }
+
+        [Required]
+        public string MimeType { get; set; }
+
+        [Required]
+        public string Data { get; set; }
+    }
+}
