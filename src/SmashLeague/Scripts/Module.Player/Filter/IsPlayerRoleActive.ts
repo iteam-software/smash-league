f2d71468233376df6ed1b770b68ec0a6213c324d
Matt Lynam <mlynam@iteamnm.com>
2015-09-01 17:27:53 -0600

    git cache fix

---
+++ src/SmashLeague/Scripts/Module.Player/Filter/IsPlayerRoleActive.ts
@@ -0,0 +1,21 @@
+﻿
+module SmashLeague {
+  'use strict';
+
+  export function IsPlayerRoleActive() {
+    return (field: number, role: string) => {
+      switch (role.toLowerCase()) {
+        case 'tank':
+          return (field & 0x1) == 0x1;
+        case 'assassin':
+          return (field & 0x2) == 0x2;
+        case 'support':
+          return (field & 0x4) == 0x4;
+        case 'specialist':
+          return (field & 0x8) == 0x8;
+      }
+    }
+  }
+
+  Application.Module.filter('isPlayerRoleActive', IsPlayerRoleActive);
+}
